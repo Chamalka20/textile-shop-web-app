bbcdd62e47b7f5aeaf823bc44d0e04b4dfb80873
chamalka <chamalka@Chamalkadell>
2022-10-04 19:52:16 +0530

    product add

--- textile shop web app/src/main/java/com/util/product_DB.java
+++ textile shop web app/src/main/java/com/util/product_DB.java
@@ -1,13 +1,38 @@
 package com.util;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class product_DB {
 
-	public static boolean insert_pro() {
+	public  boolean insert_pro(String name,String desc,String price) {
+		boolean isSuccess = false; 
+		
+		DB_connect db = new DB_connect();
+		Connection con = null; 
+		String sql ="INSERT INTO `textile`.`product` (`name`, `desc`, `price`) VALUES ('"+name+"', '"+desc+"', '"+price+"');";
+		
+		try {
+			
+			con=db.getConnection();
+			PreparedStatement stmt = con.prepareStatement(sql);
+			int rs = stmt.executeUpdate();
+			
+			if(rs>0) {
+				
+				System.out.println("success");
+				isSuccess=true;
+			}else {
+				System.out.println("not success");
+			}
+			
+		}catch(Exception e) {
+			
+			e.printStackTrace();
+		}
 		
 		
-		
+		return isSuccess;
 	}
-	
-	
-	
+		
 }
---
+++ textile shop web app/src/main/webapp/admin.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<h2>add products</h2>
+	<form action="pro_add" method="post">
+	
+		Name<input type="text" name="name"><br>
+		Description<input type="text" name="desc"><br>
+		Price<input type="text" name="price"><br>
+		<input type="submit">
+	
+	</form>
+
+</body>
+</html>   
