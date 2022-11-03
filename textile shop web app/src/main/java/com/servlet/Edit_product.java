f7dbe5948a85fe3620aa40c340bd28eef0ff0034
chamalka <chamalka@Chamalkadell>
2022-11-03 18:14:58 +0530

    up

---
+++ textile shop web app/src/main/java/com/servlet/Edit_product.java
@@ -0,0 +1,24 @@
+package com.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Edit_product")
+public class Edit_product extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+  
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		
+		
+		
+	}
+
+}
